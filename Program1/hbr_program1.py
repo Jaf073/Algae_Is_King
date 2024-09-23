640da3688e610a3aee22ada6549a028a70ea5486
haydensgithub <haydenbrimage@gmail.com>
2024-09-23 13:49:54 -0500

    program1_first_iteration

--- Program1/hbr_program1.py
+++ Program1/hbr_program1.py
@@ -18,14 +18,35 @@
 import sys
 
 
-def BinaryDecode(bit):
-    a = 5
+def BinaryDecode(code, bit):
+    # We first convert the string to an array and remove the newline character
+    ar = list(code)
+    del ar[-1]
+    
+    # variable setup #
+    decrypted = [] # we will store solved elements of the message here
+    tempstr = "" # we will store binary characters here until they are ready to be solved
+    group = 0 # this will keep track of if we've reached the bit size
+    index = 0 # iterates through the entire array once
+
+    # we need to split into chunks of size bit
+    while index < len(ar):
+        tempstr += ar[index]
+        index += 1
+        group += 1
+        if group >= bit: # this means tempstring has the character in binary
+            # FIXME: I don't know if this works with backspace, we can hardcode an if statement if it doesn't
+            decrypted += "".join([chr(int(tempstr, 2))]) # we cast the string to an int, then the int to it's respective char
+            
+            tempstr = ""
+            group = 0
+    printstr = "".join(decrypted)
+    print(printstr)
 
 def main():
     inp = sys.stdin.read()
-    print(inp)
-
-
+    BinaryDecode(inp, 7)
+    BinaryDecode(inp, 8)
 
 
 main()
