3913f32cc0e6d7f218fa55cc536293662b3ba7ab
haydensgithub <haydenbrimage@gmail.com>
2024-09-23 12:58:56 -0500

    Program1Setup

---
+++ Program1/hbr_program1.py
@@ -0,0 +1,32 @@
+# we create an array equivalent to an ascii table
+
+#we must first take in input from the command line
+
+# we then split the string into 8 char arrays
+
+# we convert the eight character array into a value using a binary conversion 
+
+# we add the corresponded element of the ascii array to a blank string
+# IF the value is equal to 8 (backspace), we subtract from the array
+
+# we output the string using stdout
+
+# repeat the process using 7 char arrays
+
+# to this end, we'll build the functionality into a function that takes the amount of characters as an argument
+
+import sys
+
+
+def BinaryDecode(bit):
+    a = 5
+
+def main():
+    inp = sys.stdin.read()
+    print(inp)
+
+
+
+
+main()
+
