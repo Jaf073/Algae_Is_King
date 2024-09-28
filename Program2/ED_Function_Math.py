9696eac7957edff66fe1ce3690b22287fbd6569f
Jaf073 <147427450+Jaf073@users.noreply.github.com>
2024-09-28 17:15:27 -0500

    Add files via upload

--- Program2/ED_Function_Math.py
+++ Program2/ED_Function_Math.py
@@ -3,22 +3,28 @@ def generate_key(msg, key):
     #Force key into lowercase only
     key = key.lower()
     keyList = []
-    buff = 0
+    buff = 0 #make a buffer to keep iteration on track if we encounter some char not a-z
     for i in range(len(msg)):
+        #Skip any character that is not a-z
         if (ord(key[(i+buff)%len(key)]) >= 97 and ord(key[(i+buff)%len(key)]) <=122):
-            keyList.append(key[((i+buff)%len(key))])
+            #add char to keylist the same length as the msg
+            keyList.append(key[((i+buff)%len(key))]) 
         else:
             buff += 1
+            #add char to keylist the same length as the msg
             keyList.append(key[((i+buff)%len(key))])
     return "".join(keyList)
 
-def encrypt_vigenere(msg, key):
+def Encrypt_Vig(msg, key):
     encrypted_text = []
+    #make a buffer to keep iteration on track if we encounter some char not a-z
     buff = 0
+    #get key same length as list
     key = generate_key(msg, key)
     for i in range(len(msg)):
         char = msg[i]
         if char.isupper():
+            #Fun math to find char w/o using a chart
             encrypted_char = chr((ord(char.lower()) + ord(key[i+buff]) - 2 * 97) % 26 + 97).upper()
         elif char.islower():
             encrypted_char = chr((ord(char) + ord(key[i+buff]) - 2 * 97) % 26 + 97)
@@ -29,14 +35,17 @@ def encrypt_vigenere(msg, key):
         encrypted_text.append(encrypted_char)
     return "".join(encrypted_text)
 
-def decrypt_vigenere(msg, key):
+def Decrypt_Vig(msg, key):
     decrypted_text = []
+    #make a buffer to keep iteration on track if we encounter some char not a-z
     buff = 0
+    #get key same length as list
     key = generate_key(msg, key)
     for i in range(len(msg)):
         char = msg[i]
+        #force char to be in lowercase for math but keep origional to determine if it is capitalized later
         tempChar = char.lower()
-        if char.isupper():
+        if char.isupper(): #Fun math to find char w/o using a chart
             decrypted_char = chr((ord(tempChar) - ord(key[i+buff]) + 26) % 26 + 97).upper()
         elif char.islower():
             decrypted_char = chr((ord(tempChar) - ord(key[i+buff]) + 26) % 26 + 97)
@@ -46,17 +55,14 @@ def decrypt_vigenere(msg, key):
         decrypted_text.append(decrypted_char)
     return "".join(decrypted_text)
 
-# Example usage
+#Test Case
 msg = "Get ready for Cyber Storm! We’re going to turn your world upside down on November 8!"
 key = "This is my key"
 
 print("MSG: " + msg)
 print("Key: " + key)
-encrypted_text = encrypt_vigenere(msg, key)
+encrypted_text = Encrypt_Vig(msg, key)
 print(f"Encrypted Text: {encrypted_text}")
 
-decrypted_text = decrypt_vigenere(encrypted_text, key)
+decrypted_text = Decrypt_Vig(Encrypt_Vig, key)
 print(f"Decrypted Text: {decrypted_text}")
-
-#previous code was only support the upper case letters
-#this code can be apply on both
