75638c1883f1818df416e6c1313ef2382b449124
gabethomp <128546274+gabethomp@users.noreply.github.com>
2024-10-07 19:04:09 -0500

    Update webScraper.py

--- Take_Home_Challenge/webScraper.py
+++ Take_Home_Challenge/webScraper.py
@@ -4,22 +4,20 @@ from bs4 import BeautifulSoup
 def scraper(url):
     # requests url info
     r = requests.get(url)
-
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser")
         
-        # finds all links on page and prints them out
-        links = soup.find_all("a")
-        for link in links:
-            href = link.get("href")
-            if href:
-                print(href)
-    else:
-        print(f"failed to retrieve webpage info. status code {r.status_code}")
+    # finds all links on page and prints them out
+    links = soup.find_all("a")
+    for link in links:
+        href = link.get("href")
+        if href:
+            print(href)
     
 
 def main():
+    # website that the scraper goes though
     website = "https://coes.latech.edu/facultystaff-directory/"
+
     scraper(website)
     
 main()
