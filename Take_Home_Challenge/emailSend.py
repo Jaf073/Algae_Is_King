15c527be27cb2ae1016eea48903226c7e6e4e1fe
Tim Farley <118396419+PhoenixFlyer@users.noreply.github.com>
2024-10-10 00:10:32 -0500

    replaces the names and adds IESB picture

--- Take_Home_Challenge/emailSend.py
+++ Take_Home_Challenge/emailSend.py
@@ -1,30 +1,56 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+from splitInfo_02 import splitInfo
 
 emailSender = '' # put your own here while testing
 
-senderPass = ''  # NOTE: You want to use something called an "app password" for this feild, it's something that you can set up with gmails ONLY if you have two factor authentication enabled
+senderPass = ''  # NOTE: You want to use something called an "app password" for this field, it's something that you can set up with gmails ONLY if you have two factor authentication enabled
 # obviously don't leave this filled in when you push to git
 
 emailVictim = '' # put somebody you don't like here while testing
 
-subject = 'This is a test email subject'
-body = """
-This is a test email body
-This should be a seperate line
-"""
+Dic1, Dic2, Dic3 = splitInfo('finished.txt')
 
-email = EmailMessage()
-email['From'] = emailSender
-email['To'] = emailVictim
-email['Subject'] = subject
-email.set_content(body)
+with open('email_template.txt', 'r') as file:
+          email_template = file.read()
+
+def replaceEmails(name_dic):
+    filledOutEmails = []
+    for name, emailVictim in name_dic.items():
+        email = EmailMessage()
+        email['From'] = emailSender
+        email['To'] = emailVictim
+        email['Subject'] = 'This is a test email subject'
+
+        contents = email_template
+        contents = contents.replace('[target]', name)
+        contents = contents.replace('fakeemail@email.com', emailSender)
+        contents = contents.replace('[your worst nightmare]', emailSender)
+
+        email.set_content(contents)
+
+        with open('IESB.jpeg', 'rb') as img:
+            email.add_attachment(img.read(), maintype='image', subtype='jpeg', filename='IESB.jpeg')
+
+        filledOutEmails.append(email)
+
+    return filledOutEmails
+
+preppedEmails = []
+preppedEmails.extend(replaceEmails(Dic1))
+preppedEmails.extend(replaceEmails(Dic2))
+preppedEmails.extend(replaceEmails(Dic3))
+
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(emailSender, senderPass)
+for email in preppedEmails:
+    emailVictim = email['To']
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(emailSender, senderPass)
     
-    # uncommenting the following command will send the email out
-    # DO NOT UNCOMMENT THIS DURING LARGE SCALE TESTING
-    # smtp.sendmail(emailSender, emailVictim, email.as_string())
+        # uncommenting the following command will send the email out
+        # DO NOT UNCOMMENT THIS DURING LARGE SCALE TESTING
+        #emailVictim = '' # if you want to do your own. put a 'break' after the next line
+        #smtp.sendmail(emailSender, emailVictim, email.as_string())
