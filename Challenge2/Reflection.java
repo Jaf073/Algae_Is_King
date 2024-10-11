eff61eaf2189dd0716a2a11a62bb1e8536e1e784
Tim Farley <118396419+PhoenixFlyer@users.noreply.github.com>
2024-10-11 13:10:02 -0500

    reflection

--- Challenge2/Reflection.java
+++ Challenge2/Reflection.java
@@ -2,12 +2,19 @@ import java.lang.reflect.*;
         
 public class Reflection {
    public static void main(String args[]) throws Exception{
-         Hint h = new Hint();
 
-         Class c = h.getClass();
+         // Comment out this block after getting what you need, then uncomment the other section
+         Class c = Class.forName(args[0]);
+         Method m[] = c.getDeclaredMethods();
+         for (int i = 0; i < m.length; i++)
+         System.out.println(m[i].toString());
 
-         Method methodcall = c.getDeclaredMethod("superprivatefunction");
-         methodcall.setAccessible(true);
-         methodcall.invoke(h);
+         // Hint h = new Hint();
+         // Class c = h.getClass();
+
+         // Method methodcall = c.getDeclaredMethod("superprivatefunction");
+         // methodcall.setAccessible(true);
+         // h.setLength(1000);
+         // methodcall.invoke(h);
    }
 }
