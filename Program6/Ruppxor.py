7bd666017022592758562da0d0498b2b86dc0780
B3P15 <andrewrupp04@gmail.com>
2024-10-16 14:41:20 -0500

    Updating xor file (not working) and adding required files for testing

--- Program6/Ruppxor.py
+++ Program6/Ruppxor.py
@@ -2,21 +2,25 @@ import sys
 
 def xor():
     # Read key from key file as bineary
-    # f = open("key", 'r')
-    # key = f.read()
-    key = '0101'
+    f = open("key", 'r')
+    key = f.read()
+    # key = '0101'
+
     # Take message from stdin
-    m = sys.stdin
-    b = bytearray()
-    message = b.extend(m)
+    message = bytearray((input().encode('utf-8')))
     
     # xor the input with the key to create the output
-    output = ''
+    out = ''
     for i in range(len(key)):
-        output.append(bool(key[i]) ^ bool(message[i]))
+        if (bool(key[i]) ^ bool(message[i])) == True:
+            out += '1'
+        else:
+            out += '0'
+
+    output = bytearray((out.encode('utf-8')))
     
     # return the output to stdout
-    print(message)
+    sys.stdout.buffer.write(output)
     
 xor()
     
