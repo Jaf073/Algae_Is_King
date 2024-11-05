4162fbad0791d39ecf7e133ace46b5bb3fe1bf3c
Jaf073 <147427450+Jaf073@users.noreply.github.com>
2024-11-05 13:47:16 -0600

    Update Steg_01.py

--- Challenge5/Steg_01.py
+++ Challenge5/Steg_01.py
@@ -18,36 +18,6 @@ def storeByte(wrapper, hidden, offset, interval):
         offset += interval
 
     return wrapper
-    
-def BinaryDecode(code, bit):
-    # We first convert the string to an array and remove the newline character
-    bruh = bin(int.from_bytes(code, byteorder=sys.byteorder))
-    ar = list(bruh)
-    del ar[-1]
-    
-    # variable setup #
-    decrypted = [] # we will store solved elements of the message here
-    tempstr = "" # we will store binary characters here until they are ready to be solved
-    group = 0 # this will keep track of if we've reached the bit size
-    index = 0 # iterates through the entire array once
-
-    # iterates through the entire array while 
-    # repeatedly building chunks of size bit
-    while index < len(ar):
-        tempstr += ar[index]
-        index += 1
-        group += 1
-        if group >= bit: # this means tempstr is ready to convert
-            decrypted += "".join([chr(int(tempstr, 2))]) # we cast the string to an int, then the int to it's respective char
-            
-            # we clear temstr and group to start again
-            tempstr = ""
-            group = 0
-    
-    # we merge the array to a string and print it
-    printstr = "".join(decrypted)
-    print(printstr)
-
 
 # get bytes from image
 def retrieveByte(wrapper, offset, interval):
